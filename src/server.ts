552df7b26fd77a44f7cbaa4a998b9a48a6fd770b
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-04-08 07:28:29 +0300

    configure the Prettier code formatter

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import Koa from 'koa';
-import Router from 'koa-router';
+import Koa from "koa";
+import Router from "koa-router";
 
 /* Create a Koa application instance. */
 const app: Koa = new Koa();
@@ -7,8 +7,8 @@ const app: Koa = new Koa();
 /* Configure the application instance to use a router middleware. */
 const router: Router = new Router();
 
-router.get('/', (ctx: Koa.Context) => {
-    ctx.body = { msg: 'Hello World!' };
+router.get("/", (ctx: Koa.Context) => {
+  ctx.body = { msg: "Hello World!" };
 });
 
 app.use(router.allowedMethods());
@@ -16,4 +16,6 @@ app.use(router.allowedMethods());
 app.use(router.routes());
 
 /* Create and start an HTTP server. */
-app.listen(3000, () => { console.log(`Server listening on port 3000`); });
+app.listen(3000, () => {
+  console.log(`Server listening on port 3000`);
+});
