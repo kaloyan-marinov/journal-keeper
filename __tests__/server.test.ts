89424d3ec0bad385f26bb6894af81d0091621933
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-04-09 07:38:06 +0300

    replace the sample test with a test of the web application (npm install supertest && npm install --save-dev @types/supertest)

--- __tests__/server.test.ts
+++ __tests__/server.test.ts
@@ -1,3 +1,4 @@
+import request from "supertest";
 import server from "../src/server";
 
 afterEach((done) => {
@@ -5,9 +6,17 @@ afterEach((done) => {
   done();
 });
 
-describe("/api/users", () => {
-  test("should verify that 2 = 2", () => {
-    const a = 2;
-    expect(a).toEqual(2);
+describe("GET /api/users", () => {
+  test("should return all User resources", async () => {
+    const response = await request(server).get("/api/users");
+
+    expect(response.status).toEqual(200);
+    expect(response.type).toEqual("application/json");
+    expect(response.body).toEqual({
+      "1": {
+        id: 1,
+        username: "jd",
+      },
+    });
   });
 });
