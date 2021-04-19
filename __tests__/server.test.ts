25f56f5e9e56419e4f0fc4cf554f251ecc1b507e
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-04-19 06:34:37 +0300

    2: increase the test coverage of the handler for POST requests to /api/users

--- __tests__/server.test.ts
+++ __tests__/server.test.ts
@@ -29,50 +29,171 @@ afterEach((done) => {
 });
 
 describe("POST /api/users", () => {
-  test("should create a new User resource", async () => {
-    const response = await request(server).post("/api/users").send({
-      username: "ms",
-      name: "Mary Smith",
-      email: "mary.smith@protonmail.com",
-      password: "456",
-    });
+  test(
+    "if a client request doesn't include a 'Content-Type: application/json' header," +
+      " the server should respond with a 400",
+    async () => {
+      const response = await request(server)
+        .post("/api/users")
+        .set("Content-Type", "text/html");
 
-    expect(response.status).toEqual(201);
-    expect(response.headers.location).toEqual("/api/users/1");
-    expect(response.type).toEqual("application/json");
-    expect(response.body).toEqual({
-      id: 1,
-      username: "ms",
-    });
+      expect(response.status).toEqual(400);
+      expect(response.type).toEqual("application/json");
+      expect(response.body).toEqual({
+        error: "Your request did not include a 'Content-Type: application/json' header",
+      });
+    }
+  );
 
-    const usersRepository: Repository<User> = connection.getRepository(User);
-    const users = await usersRepository.find();
-    expect(users.length).toEqual(1);
-    const { id, username, name, email, password } = users[0];
-    expect({
-      id,
-      username,
-      name,
-      email,
-      password,
-    }).toEqual({
-      id: 1,
-      username: "ms",
-      name: "Mary Smith",
-      email: "mary.smith@protonmail.com",
-      password: "456",
-    });
-  });
+  test(
+    "if a client doesn't send all required fields," +
+      " the server should respond with a 400",
+    async () => {
+      const completeUserPayload = {
+        username: "jd",
+        name: "John Doe",
+        email: "john.doe@protonmail.com",
+        password: "123",
+      };
+
+      interface IIncompleteUserPayload {
+        [index: string]: string;
+      }
+
+      for (let field in completeUserPayload) {
+        let incompleteUserPayload: IIncompleteUserPayload = { ...completeUserPayload };
+        delete incompleteUserPayload[field];
+
+        const response = await request(server)
+          .post("/api/users")
+          .send(incompleteUserPayload);
+
+        expect(response.status).toEqual(400);
+        expect(response.type).toEqual("application/json");
+        expect(response.body).toEqual({
+          error: `Your request body did not specify a '${field}'`,
+        });
+      }
+    }
+  );
+
+  test(
+    "if a client requests to create a new User resource," +
+      " the server should create that resource",
+    async () => {
+      const response = await request(server).post("/api/users").send({
+        username: "ms",
+        name: "Mary Smith",
+        email: "mary.smith@protonmail.com",
+        password: "456",
+      });
+
+      expect(response.status).toEqual(201);
+      expect(response.headers.location).toEqual("/api/users/1");
+      expect(response.type).toEqual("application/json");
+      expect(response.body).toEqual({
+        id: 1,
+        username: "ms",
+      });
+
+      const usersRepository: Repository<User> = connection.getRepository(User);
+      const users = await usersRepository.find();
+      expect(users.length).toEqual(1);
+      const { id, username, name, email, password } = users[0];
+      expect({
+        id,
+        username,
+        name,
+        email,
+        password,
+      }).toEqual({
+        id: 1,
+        username: "ms",
+        name: "Mary Smith",
+        email: "mary.smith@protonmail.com",
+        password: "456",
+      });
+    }
+  );
+
+  test(
+    "if a client requests to create a new User resource" +
+      " with a username which coincides with that of an existing User," +
+      " the server should respond with a 400",
+    async () => {
+      const response1 = await request(server).post("/api/users").send({
+        username: "jd",
+        name: "John Doe",
+        email: "john.doe@protonmail.com",
+        password: "123",
+      });
+
+      const response2 = await request(server).post("/api/users").send({
+        username: "jd",
+        name: "different-name",
+        email: "different-email",
+        password: "different-password",
+      });
+
+      expect(response2.status).toEqual(400);
+      expect(response2.type).toEqual("application/json");
+      expect(response2.body).toEqual({
+        error:
+          "There already exists a User resource with the username that you provided",
+      });
+
+      const usersRepository: Repository<User> = connection.getRepository(User);
+      const users: User[] = await usersRepository.find();
+      expect(users.length).toEqual(1);
+      expect(users[0].name).toEqual("John Doe");
+    }
+  );
+
+  test(
+    "if a client requests to create a new User resource" +
+      " with an email which coincides with that of an existing User," +
+      " the server should respond with a 400",
+    async () => {
+      const response1 = await request(server).post("/api/users").send({
+        username: "jd",
+        name: "John Doe",
+        email: "john.doe@protonmail.com",
+        password: "123",
+      });
+
+      const response2 = await request(server).post("/api/users").send({
+        username: "different-username",
+        name: "different-name",
+        email: "john.doe@protonmail.com",
+        password: "different-password",
+      });
+
+      expect(response2.status).toEqual(400);
+      expect(response2.type).toEqual("application/json");
+      expect(response2.body).toEqual({
+        error: "There already exists a User resource with the email that you provided",
+      });
+
+      const usersRepository: Repository<User> = connection.getRepository(User);
+      const users: User[] = await usersRepository.find();
+      expect(users.length).toEqual(1);
+      expect(users[0].name).toEqual("John Doe");
+    }
+  );
 });
 
 describe("GET /api/users", () => {
-  test("should return all User resources", async () => {
-    const response = await request(server).get("/api/users");
+  test(
+    "if a client requests all User resources," +
+      " the server should respond with public representations of all of them",
+    async () => {
+      const response = await request(server).get("/api/users");
 
-    expect(response.status).toEqual(200);
-    expect(response.type).toEqual("application/json");
-    expect(response.body).toEqual({
-      users: [],
-    });
-  });
+      expect(response.status).toEqual(200);
+      expect(response.type).toEqual("application/json");
+      expect(response.body).toEqual({
+        users: [],
+      });
+    }
+  );
 });
