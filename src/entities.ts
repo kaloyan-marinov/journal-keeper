1464174309f9e64c93f07ae0b7c6d930c27aab4c
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-04-21 06:42:48 +0300

    5: implement an Entry entity, which has a many-to-one relationship to the User entity

--- src/entities.ts
+++ src/entities.ts
@@ -4,6 +4,9 @@ import {
   Column,
   CreateDateColumn,
   UpdateDateColumn,
+  OneToMany,
+  ManyToOne,
+  JoinColumn,
 } from "typeorm";
 
 @Entity({ name: "users" })
@@ -50,4 +53,50 @@ export class User {
     name: "updated_at",
   })
   updatedAt?: Date;
+
+  @OneToMany(() => Entry, (entry: Entry) => entry.user, {
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE",
+  })
+  entries?: Array<Entry>;
+}
+
+@Entity({ name: "entries" })
+export class Entry {
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column({
+    nullable: false,
+  })
+  timestamp?: string;
+
+  @Column({
+    nullable: false,
+  })
+  content?: string;
+
+  @CreateDateColumn({
+    default: () => "CURRENT_TIMESTAMP",
+    type: "datetime",
+    name: "created_at",
+  })
+  createdAt?: Date;
+
+  @UpdateDateColumn({
+    default: () => "CURRENT_TIMESTAMP",
+    type: "datetime",
+    name: "updated_at",
+  })
+  updatedAt?: Date;
+
+  @Column({
+    nullable: false,
+    name: "user_id",
+  })
+  userId?: number;
+
+  @ManyToOne(() => User, (user: User) => user.entries, { onDelete: "CASCADE" })
+  @JoinColumn({ name: "user_id" })
+  user?: User;
 }
