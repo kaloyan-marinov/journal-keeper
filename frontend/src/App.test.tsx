5a34b77ca22f11f23d86f76caa6ac98ec867af7a
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-05-01 08:16:23 +0300

    11: create routes "on the client side"

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -6,6 +6,9 @@ describe("<App>", () => {
   test("initial render (i.e. before/without any user interaction)", () => {
     const { getByText } = render(<App />);
 
+    getByText("Home");
+    getByText("Sign Up");
+
     getByText("Welcome to MyMonthlyJournal!");
   });
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
 const App = () => {
-  return <div>Welcome to MyMonthlyJournal!</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Link to="/">Home</Link> | <Link to="/sign-up">Sign Up</Link>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <div>Welcome to MyMonthlyJournal!</div>
+        </Route>
+        <Route exact path="/sign-up">
+          <div>Create a new account!</div>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
