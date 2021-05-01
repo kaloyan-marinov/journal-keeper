6d748127d9ea0a950fdf16267b3106f11acef4b6
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-05-01 07:58:32 +0300

    11: modify the previous commit's test so as to align it more closely with React Testing Library's philosophy of 'test each (frontend-app) component in the way that a user would use it'

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -4,13 +4,8 @@ import App from "./App";
 
 describe("<App>", () => {
   test("initial render (i.e. before/without any user interaction)", () => {
-    /*
-    TODO: update this test case, as suggested by https://stackoverflow.com/questions/54234515/get-by-html-element-with-react-testing-library
-    */
-    const { container } = render(<App />);
+    const { getByText } = render(<App />);
 
-    const divs = container.querySelectorAll("div");
-
-    expect(divs.length).toEqual(1);
+    getByText("Hello world!");
   });
 });
