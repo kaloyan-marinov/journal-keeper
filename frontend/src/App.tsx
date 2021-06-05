592374211e6005cb3031eb61d3726210ca4d8280
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-06-05 15:03:56 +0300

    11: make <DeleteEntryLink> handle the onMouseEnter and onMouseLeave (browser) events by, respectively, displaying and hiding a hint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1507,7 +1507,31 @@ const SingleEntry = (props: SingleEntryProps) => {
 
 export const DeleteEntryLink = (props: { to: string }) => {
   const targetURL: string = props.to;
-  return <Link to={targetURL}>Delete</Link>;
+
+  console.log(
+    `${new Date().toISOString()}` +
+      ` - ${__filename}` +
+      ` - React is rendering <DeleteEntryLink to=${targetURL}>`
+  );
+
+  const [shouldDisplayHint, setShouldshouldDisplayHint] = React.useState(false);
+
+  const hint = (
+    <span> (HINT: After clicking, you will be asked to confirm your choice.)</span>
+  );
+
+  return (
+    <React.Fragment>
+      <Link
+        to={targetURL}
+        onMouseEnter={() => setShouldshouldDisplayHint(true)}
+        onMouseLeave={() => setShouldshouldDisplayHint(false)}
+      >
+        Delete
+      </Link>
+      {shouldDisplayHint && hint}
+    </React.Fragment>
+  );
 };
 
 const offsetsFromUtc = () => {
