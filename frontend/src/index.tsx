afc21ad5537f703dc708bf84c28749d41f2cad92
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-09-02 07:46:56 +0300

    14: extract `store.tsx` out of `App.tsx` (which breaks all but 2 test cases)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+
 import { Provider } from "react-redux";
-import { store } from "./App";
+import { store } from "./store";
 import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
