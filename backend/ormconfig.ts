46d7ab129b4a30c80d26a80a0a781891a6c0697d
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-09-11 15:48:16 +0300

    19: get `npm run dev` to work, but note that issuing `npm run migration:run -- -c connection-to-db-for-dev` fails with "You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '"users" (...) at line 1'"

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -3,21 +3,22 @@ import { ConnectionOptions } from "typeorm";
 import path from "path";
 
 config();
-const { DATABASE_URL } = process.env;
-console.log(
-  `${new Date().toISOString()} -` +
-    ` ${__filename}` +
-    ` - inspecting the environment variable DATABASE_URL:`
-);
-console.log(DATABASE_URL);
-if (DATABASE_URL === undefined) {
-  console.log(
-    `${new Date().toISOString()} -` +
-      ` ${__filename} -` +
-      ` no environment variable DATABASE_URL has been found - aborting!`
-  );
-  process.exit(1);
-}
+
+// const { DATABASE_URL } = process.env;
+// console.log(
+//   `${new Date().toISOString()} -` +
+//     ` ${__filename}` +
+//     ` - inspecting the environment variable DATABASE_URL:`
+// );
+// console.log(DATABASE_URL);
+// if (DATABASE_URL === undefined) {
+//   console.log(
+//     `${new Date().toISOString()} -` +
+//       ` ${__filename} -` +
+//       ` no environment variable DATABASE_URL has been found - aborting!`
+//   );
+//   process.exit(1);
+// }
 
 let sourceCodeFolder: string = process.env.NODE_ENV === "production" ? "dist" : "src";
 console.log(
@@ -30,8 +31,12 @@ console.log(sourceCodeFolder);
 const connectionsOptionsObjects: ConnectionOptions[] = [
   {
     name: "connection-to-db-for-dev",
-    type: "sqlite",
-    database: DATABASE_URL,
+    type: process.env.DATABASE_TYPE as "mysql",
+    host: process.env.DATABASE_HOSTNAME,
+    port: parseInt(process.env.DATABASE_PORT as string),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     entities: [path.join(sourceCodeFolder, "entities.*")],
     cli: {
       migrationsDir: path.join(sourceCodeFolder, "migration"),
