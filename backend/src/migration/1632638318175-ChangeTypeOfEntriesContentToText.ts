8e5920b4a04642d5a98a4e975c34c2378a8328eb
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-09-26 10:11:12 +0300

    22: issue `npm run migration:generate -- -c connection-to-db-for-dev -n ChangeTypeOfEntriesContentToText` (but applying that migration fails)

---
+++ backend/src/migration/1632638318175-ChangeTypeOfEntriesContentToText.ts
@@ -0,0 +1,24 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class ChangeTypeOfEntriesContentToText1632638318175 implements MigrationInterface {
+    name = 'ChangeTypeOfEntriesContentToText1632638318175'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query("ALTER TABLE `users` CHANGE `created_at` `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP");
+        await queryRunner.query("ALTER TABLE `users` CHANGE `updated_at` `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(6)");
+        await queryRunner.query("ALTER TABLE `entries` DROP COLUMN `content`");
+        await queryRunner.query("ALTER TABLE `entries` ADD `content` text NOT NULL");
+        await queryRunner.query("ALTER TABLE `entries` CHANGE `created_at` `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP");
+        await queryRunner.query("ALTER TABLE `entries` CHANGE `updated_at` `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(6)");
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query("ALTER TABLE `entries` CHANGE `updated_at` `updated_at` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP");
+        await queryRunner.query("ALTER TABLE `entries` CHANGE `created_at` `created_at` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP");
+        await queryRunner.query("ALTER TABLE `entries` DROP COLUMN `content`");
+        await queryRunner.query("ALTER TABLE `entries` ADD `content` varchar(255) NOT NULL");
+        await queryRunner.query("ALTER TABLE `users` CHANGE `updated_at` `updated_at` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP");
+        await queryRunner.query("ALTER TABLE `users` CHANGE `created_at` `created_at` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP");
+    }
+
+}
