ddd2614311b7f973b8a096bc0825a67e17abe469
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-09-26 10:13:48 +0300

    22: modify the migration script so that it can be run without failing, but notice that it has the following bug: its application loses the content of everything in the `entries.content` column

--- backend/src/migration/1632638318175-ChangeTypeOfEntriesContentToText.ts
+++ backend/src/migration/1632638318175-ChangeTypeOfEntriesContentToText.ts
@@ -4,12 +4,12 @@ export class ChangeTypeOfEntriesContentToText1632638318175 implements MigrationI
     name = 'ChangeTypeOfEntriesContentToText1632638318175'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `users` CHANGE `created_at` `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP");
-        await queryRunner.query("ALTER TABLE `users` CHANGE `updated_at` `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(6)");
+        await queryRunner.query("ALTER TABLE `users` CHANGE `created_at` `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP");
+        await queryRunner.query("ALTER TABLE `users` CHANGE `updated_at` `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP");
         await queryRunner.query("ALTER TABLE `entries` DROP COLUMN `content`");
         await queryRunner.query("ALTER TABLE `entries` ADD `content` text NOT NULL");
-        await queryRunner.query("ALTER TABLE `entries` CHANGE `created_at` `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP");
-        await queryRunner.query("ALTER TABLE `entries` CHANGE `updated_at` `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(6)");
+        await queryRunner.query("ALTER TABLE `entries` CHANGE `created_at` `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP");
+        await queryRunner.query("ALTER TABLE `entries` CHANGE `updated_at` `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP");
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
